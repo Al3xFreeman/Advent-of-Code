f7c337fae7abb701735212ebf77bb477c00aade4
Alejandro Díaz Román <alejandrodiazroman.contacto@gmail.com>
2019-12-02 20:37:41 +0100

    D2P1 Improved

--- 2019/Day 2/Python/Day2P1.py
+++ 2019/Day 2/Python/Day2P1.py
@@ -1,31 +1,21 @@
+f = [int(x) for x in open("d2.txt").read().split(",")]
 
+index = 0
+out = False
+while not out:
+    opCode = f[index]
+    first_index = f[index + 1]
+    second_index = f[index + 2]
+    placement = f[index + 3]
 
-f = open("Day 2 Problem 1 input.txt", "r")
-
-f1 = f.readlines()
-my_list_int = []
-my_list = f1[0].split(",")
-for x in my_list:
-    my_list_int.append(int(x))
-
-
-i = 0
-while i < len(my_list_int):
-    if my_list_int[i] == 1:
-        first_number = my_list_int[my_list_int[i+1]]
-        second_number = my_list_int[my_list_int[i+2]]
-        position_index = my_list_int[i+3]
-
-        my_list_int[position_index] = first_number + second_number
-    elif my_list_int[i] == 2:
-        first_number = my_list_int[my_list_int[i + 1]]
-        second_number = my_list_int[my_list_int[i + 2]]
-        position_index = my_list_int[i + 3]
-
-        my_list_int[position_index] = first_number * second_number
+    if opCode == 1:
+        f[placement] = f[first_index] + f[second_index]
+    elif opCode == 2:
+        f[placement] = f[first_index] * f[second_index]
+    #   elif opCode == 99:
+    #       out = True
     else:
-        break
+        out = True
+    index += 4
 
-    i += 4
-
-print(my_list_int[0])
+print(f[0])
