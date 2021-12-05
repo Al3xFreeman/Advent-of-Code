e9a0c92a73b2316fc4b3749b0c2f7a5932b69fdc
Alejandro Díaz Román <alejandrodiazroman.contacto@gmail.com>
2021-12-05 22:20:54 +0100

    2021 Day2P1

---
+++ 2021/Day2/Day2P1.py
@@ -0,0 +1,25 @@
+import os
+
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = "Day2P1Input.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+with open(abs_file_path) as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+    hor = 0
+    ver = 0
+
+    for line in lines:
+        dir = line.split()[0]
+        ammount = int(line.split()[1])
+
+        if(dir == "forward"):
+            hor += ammount
+        elif(dir == "up"):
+            ver -= ammount
+        else:
+            ver += ammount
+
+    print(hor * ver)
