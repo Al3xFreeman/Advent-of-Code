b0f21d3e521fef532b28b8a855e00106ae0402d7
Alejandro Díaz Román <alejandrodiazroman.contacto@gmail.com>
2021-12-18 14:46:39 +0100

    2021 Day13P2
    Esta me ha molao, súper original

---
+++ 2021/Day_13/Day13P2.py
@@ -0,0 +1,43 @@
+import os
+
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = "Day13Input.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+with open(abs_file_path) as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+    
+    points = [tuple(map(int, x.split(','))) for x in lines[:-13]]
+    
+    setPoints = set(points)
+    folds = [x.split('=') for x in lines[-12:]]
+    folds = list(map(lambda x: [x[0][-1], int(x[1])], folds))
+
+    for fold in folds:
+        newFold = set()
+        if fold[0] == 'x': #fold through axis x
+            for point in setPoints:
+                if(point[0] > fold[1]):
+                    x = fold[1] - (point[0] - fold[1]) #x
+                else:
+                    x = point[0]
+
+                newFold.add((x, point[1]))
+        else: #fold through axis y
+            for point in setPoints:
+                if(point[1] > fold[1]):
+                    y = fold[1] - (point[1] - fold[1]) #y
+                else:
+                    y = point[1]
+
+                newFold.add((point[0], y))
+        setPoints = newFold
+
+    code = [["."] * 40 for i in range(6)]
+
+    for p in setPoints:
+        code[p[1]][p[0]] = "#"
+
+    for c in code:
+        print(c)
